7a7aa74bd4f4bee0f299b9f34c0a9775f7e516ec
aliSoudani <lkollasfbt@gmail.com>
2020-02-01 09:35:24 +0100

    ali

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,32 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+function fullName(firstName,lastName){
+
+console.log(firstName+' '+lastName);}
+
+// i use this function  fullName('amir','ben youssef') it will return :amir ben youssef
+
+//2 
+function isMultiple(a,b){
+var d=a%b
+if(d===0){console.log(a+'  is a multiply of'+' '+ b)}
+else console.log(a+ ' is not a multiply of '+ b)}
+//  then we use this function in our console , isMultiple(13,3) that returns: 13 is not a multiply of 3
+
+//3
+function average(arr){
+	var s=0;
+	for(var i=0;i<arr.length;i=i+1){
+		s=s+arr[i];
+	}
+	return s/(arr.length);
+}
+//average([13,14,13,15,16,17,19,13,16,15])=15.1
+
+// 4
+function ageSecond(age){
+	return age*365*24*60*60
+}
+
+// then i used this function to calculate my age 29 in seconds, ageSecond(29)=914544000 .
