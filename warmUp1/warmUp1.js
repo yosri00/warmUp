15b528db79308f489641f35135c0064b8f567f6d
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-01 13:45:06 +0100

    yosri

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -4,3 +4,28 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+
+var LastName = Mimouni
+var FirstName = Yosri
+
+combine = LastName + FirstName
+console.log(combine)
+
+//2
+
+
+//3
+var array = [13, 14, 13, 15, 16, 17, 19, 13, 16, 15]
+var i = 0
+var result = ''
+function average(array) {
+    for (i < array.length[i])
+        result = (array(i) / array.length) + result
+    return result;
+}
+
+//44
+Myage = 2020 - 1996
+function lag(age) {
+    return (age * 365.25 * 24 * 60 * 60);
+}
