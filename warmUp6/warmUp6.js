4af50b6d8b4ab6c498fb224a640501b27feee0a2
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-07 10:04:26 +0100

    yosri

--- warmUp6/warmUp6.js
+++ warmUp6/warmUp6.js
@@ -1,20 +1,21 @@
 // 1 - Write a function named objKey that take an object as a parameter and returns an array containing the keys of the object
 // objKey({name:'ahmed',age:20}) ==> ['name','age']
-
-function objKey(obj) {
-  // your code here...
+function objKey(name, age) {
+  return Object.keys(objKey)
 }
 
 // 2 - Write a function named objLength to get the length of a JavaScript object
 // objLength({name:'ahmed',age:20}) ==> 2
 
 function objLength(obj) {
-  // your code here...
+  return Object.keys(obj).length
 }
 
 // 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
 // objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
 
+var obi = [{ id: 2, value: 50 }, { id: 0, value: 70 }, { id: 1, value: 40 }]
 function objSort(obj) {
-  // your code here...
+  obj.sort((a, b) => (a.id) - (b.id));
+  return obj
 }
