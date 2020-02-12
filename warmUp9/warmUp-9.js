ff003337f2117f9db56b979cb48b9a84c1d9b866
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-12 09:54:33 +0100

    yosri

--- warmUp9/warmUp-9.js
+++ warmUp9/warmUp-9.js
@@ -2,7 +2,12 @@
 // -name of females in your class
 // -name of males in your class
 // -name of your class instructors 
-
+var arr = ["shania", "shishi", "carlos"];
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
-
+function middle(arr) {
+    return {
+        arr.splice(2, 0, "wiwi", "mimi");
+    }
+}
+alert(arr);
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
