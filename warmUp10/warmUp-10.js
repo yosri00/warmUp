f906bad3e834dc1d7067c4123354abfadf58c122
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-13 09:55:25 +0100

    yos

--- warmUp10/warmUp-10.js
+++ warmUp10/warmUp-10.js
@@ -1,6 +1,14 @@
 // 1-Write a function that takes a string as an input and returns the reverse of each letter 
 //followed by a number starting from zero(solve it using while loop).
 // example:    reverseStr('hello'); "o1l2l3e4h"
+function reverseStr(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
 
 
 // 2-write a function that takes array of strings and returns an array of the strings that have the same length
