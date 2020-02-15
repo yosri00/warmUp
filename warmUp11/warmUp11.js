69c91d1c309db333d5d5be5cf23f907118978476
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-15 10:11:20 +0100

    yosr

--- warmUp11/warmUp11.js
+++ warmUp11/warmUp11.js
@@ -9,3 +9,30 @@
     • Write a function searchMates that, given a query and an array of Mates,
       searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 */
+function classmates(name, age, gender, favs, nationality) {
+  return {
+    fullName: name,
+    Age: age,
+    Gender: gender,
+    Favourites: favs,
+    Nationality: nationality
+  };
+}
+var classmates = [];
+function displayFriend(mate) {
+  return mate.fullName + ', is ' + mate.Age + 'years old, from ' + mate.Nationality;
+}
+function addFriend(mates, friend) {
+  mates.push(friend);
+}
+function nnumberofmales(mates) {
+  var males = 0;
+  for (var mate in mates) {
+    if (mates[mate].Gender === 'male') {
+      males++;
+    }
+  }
+  return males;
+}
+
+function searchMates
