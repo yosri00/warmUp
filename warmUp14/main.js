df1c3508c2a830d4ff4f579d245bd252790969f0
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-20 10:29:55 +0100

    yosri

---
+++ warmUp14/main.js
@@ -0,0 +1,9 @@
+  function employee (name,salary){
+    return {
+      name: name,
+      salary: salary
+    }   
+  }
+    var employeeA = employee("jack", 100);
+  var employeeB = employee("Mark", 200);
+  var employeeC = employee("Sara", 150)
